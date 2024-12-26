020d6dfbba6f3ee0ed880dd762e0eb7e7330d26b
Aashishbirhade <aashishbirhade@gmail.com>
2024-12-26 20:36:36 +0530

    Day 19

---
+++ prepinsta/112_Snace_case_pascal.py
@@ -0,0 +1,8 @@
+a ="Coding With_Aashish"
+r  = ""
+for i in a:
+    if i == " " or i == "_":
+        pass
+    else:
+        r += i
+print(r)
---
+++ prepinsta/113_delimiter.py
@@ -0,0 +1,6 @@
+a = [2,3,4,5]
+b ="*"
+s  =" "
+for i in a:
+    s += str(i) + "*"
+print(s)
