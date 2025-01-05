e40b841cfdb1caa2e73bceabe2e759543a8f4a65
Aashishbirhade <aashishbirhade@gmail.com>
2025-01-05 21:36:22 +0530

    Day 1

---
+++ JavaScript/01_random_array.js
@@ -0,0 +1,3 @@
+const a = new Array("Aashish","Moru","jayesh","sanket","raaj")
+var index = Math.round(Math.random()*a.length)
+console.log(a[index])
---
+++ JavaScript/02_foreach_array.js
@@ -0,0 +1,7 @@
+var a = [10,20,3,4,50,6]
+var v = a.map((i)=>i*10)
+console.log(v)
+
+a.forEach((i)=>{
+    console.log(i*3)
+})
