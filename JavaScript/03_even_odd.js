7428cc90a97464b33fd820319e12d84d2ed6a7f8
Aashishbirhade <aashishbirhade@gmail.com>
2025-01-06 20:56:38 +0530

    Day 2

---
+++ JavaScript/03_even_odd.js
@@ -0,0 +1,4 @@
+
+var a = [22,12,43,53,54,2,7]
+const c = a.filter((e)=> {if(e%2==0) return true})
+console.log(c)
---
+++ JavaScript/04_Date_format.js
@@ -0,0 +1,5 @@
+const a = new Date()
+day = a.getDate()
+month = a.getMonth()+1
+y = a.getFullYear()
+console.log(`${day}/${month}/${y}`)
