30bd2c2d9dc3b60e541cec3890268a5715a9e25c
Aashishbirhade <aashishbirhade@gmail.com>
2025-02-14 21:42:17 +0530

    Day 65

--- ploting/pattern/192_rev_left_tri.py
+++ ploting/pattern/192_rev_left_tri.py
@@ -1,7 +1,5 @@
 n = 5
-for i in range(n-1,0,-1):
-    for k in range(n-i):
-        print(" ",end=" ")
+for i in range(n,0,-1):
     for j in range(i):
         print("*",end=" ")
-    print("")
+    print(" ")
