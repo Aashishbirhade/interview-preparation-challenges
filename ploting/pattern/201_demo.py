10aa38d9c7cc7ece15e76475a02686d582ebc312
Aashishbirhade <aashishbirhade@gmail.com>
2025-02-17 21:25:55 +0530

    Day 69

---
+++ ploting/pattern/201_demo.py
@@ -0,0 +1,6 @@
+n = 3
+r =4
+for i in range(r):
+    for j in range(n):
+        print(n,end=" ")
+    print(" ")
---
+++ ploting/pattern/202_concave_tri.py
@@ -0,0 +1,9 @@
+n = 5
+for i in range(n,0,-1):
+    for k in range(i):
+        print("*",end=" ")
+    print(" ")
+for i in range(n+1):
+    for k in range(i):
+        print("*",end=" ")
+    print(" ")
