ec62c394e62a52be871024a28d38efcdbee9cfed
Aashishbirhade <aashishbirhade@gmail.com>
2025-03-04 21:00:20 +0530

    Day 78

---
+++ ploting/219_pattern_A.py
@@ -0,0 +1,8 @@
+n = 6
+for i in range(n+1):
+    for j in range(n+1):
+        if j == 0 or j == n or i == 0 or i == n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print(" ")
