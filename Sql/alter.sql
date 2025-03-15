946564864074cfd7c734f1afce08d1edfedb2110
Aashishbirhade <aashishbirhade@gmail.com>
2025-03-15 21:09:23 +0530

    Day 84

--- Sql/alter.sql
+++ Sql/alter.sql
@@ -5,3 +5,7 @@ SELECT * from student;
 alter table student add score int; 
 
 select name , dept , score, grad, pass from student join class on student.roll = class.roll;
+
+select max(score), name from student  GROUP BY name;
+
+select count(pass),name from student GROUP BY name HAVING score > 100;
---
+++ ploting/pattern/226_a_b_sum.py
@@ -0,0 +1,9 @@
+i = 3
+j = 6
+su = 0
+# for c in range(i,j+1):
+#     su += c
+# print(su)
+
+b = ((j-i+1)//2*(i+j))
+print(b)
