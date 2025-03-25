14456c492ebf1e3d13689fe3663c13f27940c7cb
Aashishbirhade <aashishbirhade@gmail.com>
2025-03-25 11:10:04 +0530

    Day 88

---
+++ java/prime.java
@@ -0,0 +1,14 @@
+public class prime {
+    public static void main(String[] args) {
+        int N = 10;
+        int b = 20;
+        if(N > b) {
+            System.out.println("N is greater");
+        }
+        if (N<b) {
+           System.out.println("b is greater"); 
+        } else {
+            System.out.println("equal");
+        }
+    }
+}
