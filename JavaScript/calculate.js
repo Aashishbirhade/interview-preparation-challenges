63124f75e6a376223b93b729ba9742af4a17a31c
Aashishbirhade <aashishbirhade@gmail.com>
2025-04-08 21:38:27 +0530

    Day 100

---
+++ JavaScript/calculate.js
@@ -0,0 +1,7 @@
+
+function sum(a,b){
+    return a+b
+}
+let a = 10
+let b = 20
+console.log(sum(a,b))
---
+++ JavaScript/max_min.js
@@ -0,0 +1,5 @@
+function max_min(a){
+    return Math.max(...a),Math.min(...a)
+}
+let arr = new Array(1,2,3,4,5)
+console.log(max_min(arr))
