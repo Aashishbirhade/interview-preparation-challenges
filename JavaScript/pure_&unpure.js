07148c1f4f3a196e01137d05d0a14389c5bb201f
Aashishbirhade <aashishbirhade@gmail.com>
2025-04-09 21:44:09 +0530

    Day 100

---
+++ JavaScript/pure_&unpure.js
@@ -0,0 +1,15 @@
+// function pure(a,b){
+//     return(a+b)
+// }
+
+// let a = 10
+// let b = 20
+// console.log(pure(a,b))
+
+
+function unpure(c){
+    c++
+    return c
+}
+let c = 10
+console.log(unpure(c))
