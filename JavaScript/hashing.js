8e3e77f31a0a72c2d9b1185defac5c7a94ba80c8
Aashishbirhade <aashishbirhade@gmail.com>
2025-05-11 21:03:21 +0530

    Day 116

---
+++ JavaScript/hashing.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+
+
+app.get("/", (req, res) => {
+  res.send("hello bhau");
+});
+
+app.get("/create", async (req, res) => {
+  const { username,email, password } = req.body;
+  const salt = 10;
+  const hash = bcrypt.hash("password", salt);
+  console.log(hash);
+  const user = userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+  
+  res.send("user created",user);
+});
+app.get("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userModel.findOne({ username });
+  if (!user) {
+    return res.status(400).send("user not found");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).send("invalid credentials");
+  }
+  res.send("login successful");
+});
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
