820ce1cb4c28e53e17d1113bc37fd5ef5abf5fe1
Aashishbirhade <aashishbirhade@gmail.com>
2025-05-12 21:39:50 +0530

    Day 118

---
+++ ploting/pattern/first_odd.py
@@ -0,0 +1,15 @@
+def frequency(i):
+    dict = {}
+    for i in a:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    return dict
+n = 7
+a = ['a','b','b','b','c','c','c','a','f','c'] 
+v = frequency(a)
+for key, value in v.items():
+    if value % 2 != 0:
+        print(f"First odd-count key: '{key}' with count {value}")
+        break
