c0e010387b4a1fccee176584456af23f7689002b
Aashishbirhade <aashishbirhade@gmail.com>
2025-05-12 21:05:52 +0530

    Day 117

---
+++ ploting/pegeon_rabbit_count.py
@@ -0,0 +1,8 @@
+animal = 50
+leg = 140
+if leg < animal:
+    print("The number of legs is less than the number of animals")
+else:
+    x = (leg-2*animal)//2
+    print("rabbit count",x)
+    print("pigeon count",animal-x) 
