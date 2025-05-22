b1902d4531df879a3854f8beb4f841b233548172
Aashishbirhade <aashishbirhade@gmail.com>
2025-05-22 21:46:05 +0530

    Day 127

---
+++ JavaScript/responsive.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Responsive() {
+  return (
+   <>
+   <div>
+    <h1>Responsiveness</h1>
+     <div className='text-center text-sm md:text-lg lg:text-xl'>screen size based text size is adjust</div>
+   </div>
+   </>
+  )
+}
+
+export default Responsive
