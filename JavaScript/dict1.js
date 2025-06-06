270027a3ad6e5d1769526d9cdc4aff6544c08919
Aashishbirhade <aashishbirhade@gmail.com>
2025-06-06 20:56:11 +0530

    Day 135

--- JavaScript/dict1.js
+++ JavaScript/dict1.js
@@ -1,3 +1,8 @@
 let d = {veer:12,raaj:22,aashu:34,moru:44};
-let c = Object.entries(d).filter(([key,value])=>{return value< 24})
-console.log(c)
+// let c = Object.entries(d).filter(([key,value])=>{return value< 24})
+// console.log(c)
+for (let [key,value] of Object.entries(d)){
+    if(value < 24){
+        console.log(key);
+    }
+}
