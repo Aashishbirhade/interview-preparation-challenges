b11c8d0c3ab6121c1cf27a3bf95375fa9a04e87f
Aashishbirhade <aashishbirhade@gmail.com>
2025-06-10 22:09:51 +0530

    Day 139

---
+++ JavaScript/sinppets.js
@@ -0,0 +1,23 @@
+
+// 1
+// let a = "rem"
+// function name(){
+//      a ="veer"
+//     console.log(a)
+// }
+// name()
+
+//2
+// let a = [11,2,3,34,4]
+// a.sort((c,d) =>c-d)
+// console.log(a)
+
+//3
+const a  = {b:1}
+const b = {b:2}
+
+function objis(obj){
+    return obj.b + 1
+}
+console.log(objis(a))
+console.log(objis(b))
