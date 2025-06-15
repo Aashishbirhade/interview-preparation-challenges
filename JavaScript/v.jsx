1bfbaed0f2396298fb8efcf24d183f15eab13df5
Aashishbirhade <aashishbirhade@gmail.com>
2025-06-15 22:13:57 +0530

    Day 139

---
+++ JavaScript/v.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function v() {
+  return (
+    <>
+  
+    <div className='w-80 h-72 my-10 mx-auto border-3 border-transparent border-x-10 border-x-green-500   rounded-full'>
+        
+        <img src="./src/images/anime.png" alt="" className='h-48 mx-auto my-10  '/>
+    </div>
+    <div class="mx-auto flex min-h-screen max-w-screen-sm items-center justify-center">
+  <div class="h-36 w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+    <div className='h-full w-full bg-white'></div>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default v
