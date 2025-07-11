e3508daa9f299d0d6df1f339555a0ca4d009a931
Aashishbirhade <aashishbirhade@gmail.com>
2025-07-11 18:46:24 +0530

    Day 155

---
+++ ploting/pattern/pattern.py
@@ -0,0 +1,21 @@
+n =5 
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for k in range(2*i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+for i in range(n):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i+1):
+            print("*",end=" ")
+    print()
