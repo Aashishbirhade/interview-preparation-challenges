919fa3a297d7782745b11880a4adcb73944838dd
Aashishbirhade <aashishbirhade@gmail.com>
2025-07-17 23:00:26 +0530

    Day 159

---
+++ JavaScript/array_snipates.js
@@ -0,0 +1,8 @@
+// console.log(a)
+// var a = 10
+// var err(Undefind), let,const err(Cannot access 'a' before initialization)
+
+// console.log([]+[]) //string
+// console.log([1,2,3]+[2,3,4]) //1,2,32,3,4
+
+
