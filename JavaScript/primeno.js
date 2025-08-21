eb1bb88de69f9085d96ca45c21e07873cc2b4884
Aashishbirhade <aashishbirhade@gmail.com>
2025-08-21 20:50:01 +0530

    Day 183

--- JavaScript/primeno.js
+++ JavaScript/primeno.js
@@ -1,21 +1,40 @@
-function isprime(a){
- for (let i = 2;i<a;i++){
-    if (a%i==0){
-        return false
-    }
- }
- return true
-}
+// function isprime(a){
+//  for (let i = 2;i<a;i++){
+//     if (a%i==0){
+//         return false
+//     }
+//  }
+//  return true
+// }
 
-let a =  10
-let b = []
-let c = []
-for (let i = 2; i <= a; i++){
-    if (isprime(i)){
-        b.push(i)
-    } else {
-        c.push(i)
+// let a =  10
+// let b = []
+// let c = []
+// for (let i = 2; i <= a; i++){
+//     if (isprime(i)){
+//         b.push(i)
+//     } else {
+//         c.push(i)
+//     }
+// }
+// console.log("Prime No: ",b)
+// console.log("Non Prime No: ",c)
+
+
+function prime_no(a){
+    let f = true 
+    for(let i = 2; i<a;i++){
+        if(a%i==0){
+          f = false
+          return f 
+        }
     }
+    return f
 }
-console.log("Prime No: ",b)
-console.log("Non Prime No: ",c)
+let v = prime_no(25)
+if(v){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
