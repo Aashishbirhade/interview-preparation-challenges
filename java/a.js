622befd3c2da4826eedb7a4788007ab4f02bd3d7
Aashishbirhade <aashishbirhade@gmail.com>
2025-08-25 20:39:50 +0530

    Day 185

---
+++ java/a.js
@@ -0,0 +1,23 @@
+let a = [1,2,3,4]
+s = 5
+c_sum = 0
+
+for (let i = 0;i<a.length;i++){
+    c_sum = a[i]
+    
+    for (let j = i+1; j<a.length;j++){
+        if(c_summ == s){
+            console.log(i,(j-1))
+        }
+        if (c_sum >s|| j== a.length) 
+            break
+            c +=  arr[j]
+    }
+    console.log("no sub array ")
+}
+
+let i = 0
+let j = a.length-1
+while (i<j){
+    a[i] +a[j]
+}
